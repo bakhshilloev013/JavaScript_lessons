c57cf7778391c85f63ab2ddb8624a76397073bb3
bakhshilloev013 <88250571+bakhshilloev013@users.noreply.github.com>
2022-05-17 00:06:45 +0500

    Add files via upload

---
+++ 1. typs_of_data/js/main.js
@@ -0,0 +1,24 @@
+// Однострочные комментарии
+/* Многострочнык комментарии */
+/** JSDoc комментарии (особенные комментарии)
+ * Типы данных:
+ * 1.Примитивные -
+ * - Number
+ * - String
+ * - Boolean
+ * - null
+ * - undefined
+ * - Symbol()
+ */
+
+/**
+ * Типы данных:
+ * 2.Объекты (Reference type) передаются по ссылке -
+ * - Object: {name: "John". age: 22, isMarried: false}
+ * - Array: [1, 2, "Hello"]
+ * - Function: function foo() {};
+ * - Date: new Date()
+ */
+
+//Objects
+// console.table({name: "Thomas", age: 22, isMarried: false});
