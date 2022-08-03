770575193a1433c401b69630db4bf71602277aca
bakhshilloev013 <88250571+bakhshilloev013@users.noreply.github.com>
2022-08-03 20:06:44 +0500

    Add files via upload

---
+++ 20.1.actions_with _attributs/js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    const div = document.querySelector('div'),
+          titles = document.querySelectorAll('h1');
+    
+    // const link = div.querySelector('.link');
+    // console.log(link); 
+    // // closest Проверка на наличие родителя с классом content на несколько уровней выше чем обертываемый элемент
+
+    // div.classList.add('article', 'custom'); //Добавляем несколько классов на элемент div
+    
+    // div.classList.remove('custom'); // Удаляем указанный класс из classList
+    // console.log(div.classList);
+    // console.log(div.classList.contains('article')); // Проверка на наличие данного класса
+    // div.classList.toggle('newClass'); // Добавляет указанный, класс если его нет, а если есть класс с таким наименованием тогда он удаляется
+   
+    // div.classList.toggle('newClass'); // В данном участке кода ранее установленный класс newClass удаляется
+   
+    // console.log(link.href); // Вывод значения аттрибута href
+    // // div.setAttribute('id', 'myId') // Устанавливаем аттрибут id и следующим аргументом задаём ему значение
+    // div.id = 'newId'; // Таким способом можно устанавливать id или изменять её
+    // console.log(div.getAttribute('id')); // Возвращает значение аттрибута id
+    // console.log(div.hasAttribute('id')); // Возвращает есть-ли у элемента указанный аттрибут или нет
+    // div.removeAttribute('id'); // Удаление указанного аттрибута у элемента
+    // console.log(div.dataset);
+    // console.log(div.getAttribute('data-myAttr'));
+    // // console.clear();
+    // console.log(div);
+})
