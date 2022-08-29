85d9c936a2fb030abdc3c394f9170cb88866ccc4
bakhshilloev013 <88250571+bakhshilloev013@users.noreply.github.com>
2022-08-29 19:31:52 +0500

    Add files via upload

---
+++ 29.object_date/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+const now = new Date();
+// const now = new Date('2019-05-27'); // Июнь а не Май. Потому что в JS месяцы по счёту начинаются с 0
+// const now = new Date(2022, 5, 22, 20, 22,58); // Июнь а не Май. Потому что в JS месяцы по счёту начинаются с 0
+// const now = new Date(0);
+// const now = new Date(-999999999999999); //15
+
+// console.log(now);
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset()); // Разница между UTC и нашим временем в минутах
+// console.log(now.getTime()); // Миллисекунды с 1970 года 1 января
+
+// now.setHours(18);
+// now.setHours(18, 44);
+console.log(now);
