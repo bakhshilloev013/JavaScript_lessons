2db99008f0e2e64470f7301c630c7170272c341a
bakhshilloev013 <88250571+bakhshilloev013@users.noreply.github.com>
2022-08-29 14:29:42 +0500

    Add files via upload

---
+++ 28.setTimeout/js/script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('.btn'),
+          wrapper = document.querySelector('.wrapper'),
+          box = wrapper.querySelector('.box');
+
+    // const timeId = setTimeout(sayHello, 3000);
+    // function sayHello() {
+    //     console.log("hello");
+    // }
+
+    // const newTimeId = setTimeout(() => {
+    //     console.log("Hello world!");
+    // }, 3000);
+
+    // const lastId = setTimeout(function() {
+    //     console.log("Last one Hello");
+    // }, 3000);
+
+    // clearInterval(lastId);
+    // clearTimeout(lastId);
+
+    // const intervarId = setInterval(sayMoreHello, 3000);
+    // function sayMoreHello() {
+    //     console.log('hello');
+    // };
+
+    // const newIntervalId = setInterval(() => {
+    //     console.log('Hello world!');
+    // }, 3000);
+
+    // const lastIntervalId = setInterval(function() {
+    //     console.log('infinite hello');
+    // }, 3000);
+
+    // let i = 0;
+    // const intervalId = setInterval(() => {
+    //     console.log("Hello");
+    //     i++;
+
+    //     if (i === 5) {
+    //         clearInterval(intervalId);
+    //     }
+    // }, 500);
+
+
+    // let pos = 0;
+    // btn.addEventListener('click', () => {
+    //     function frame() {
+    //         if (pos === 300) {
+    //             clearInterval(intervalId);
+    //         } else {
+    //             pos++;
+    //             box.style.top = pos + 'px';
+    //             box.style.left = pos + 'px';
+    //         }
+    //     };
+
+    //     const intervalId = setInterval(frame, 30);
+    // })
+
+
+    
+});
